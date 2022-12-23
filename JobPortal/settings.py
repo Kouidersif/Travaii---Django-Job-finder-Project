6ec32ae4f3bfc2d95effefebf4eaee102866f376
sif <sifkouider07@gmail.com>
2022-12-23 17:26:16 +0000

    collected

--- JobPortal/settings.py
+++ JobPortal/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL= '/images/'
-STATICFILES_DIRS= [ os.path.join(BASE_DIR, 'static_files') ]
+STATICFILES_DIRS= [ os.path.join(BASE_DIR, 'staticfiles') ]
 #STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT= os.path.join(BASE_DIR, 'static/images')
