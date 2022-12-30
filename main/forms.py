98171f873dde896e49a6af66a281b587ad861236
sif <sifkouider07@gmail.com>
2022-12-30 00:11:13 +0000

    send everything

--- main/forms.py
+++ main/forms.py
@@ -77,9 +77,13 @@ class ContactForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
         self.fields['full_name'].widget.attrs['class'] = 'form-control'
+        self.fields['full_name'].widget.attrs['placeholder'] = 'Jhon Doe'
         self.fields['title'].widget.attrs['class'] = 'form-control'
+        self.fields['title'].widget.attrs['placeholder'] = 'Reason'
         self.fields['message'].widget.attrs['class'] = 'form-control'
+        self.fields['message'].widget.attrs['placeholder'] = 'Message Content'
         self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
 
 
 
